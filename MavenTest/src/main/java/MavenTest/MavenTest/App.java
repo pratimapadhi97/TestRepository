76926a845e1284db30f6427f3df0c5f07012ed43
pratimapadhi97 <38039804+pratimapadhi97@users.noreply.github.com>
2018-04-14 18:31:00 +0530

    Update App.java

--- MavenTest/src/main/java/MavenTest/MavenTest/App.java
+++ MavenTest/src/main/java/MavenTest/MavenTest/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello EveryOne2222eeeee2222");
+        System.out.println( "Hello Bibhu");
         
     }
 }
